a494d09b7bf8fd4b2ce24b60bfaa64a594e69c05
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2023-05-27 02:28:51 +0800

    Allow multi-paragraph answers in accordions
    Previously, the answers are inserted as a text node inside the accordion
    panels. To improve semantics, we can treat answers as Markdown and
    transform them to their corresponding HTML elements (so that a string of
    text is converted into a `p` tag).
    
    This also allows adding accordions with longer answers separated into
    multiple paragraphs, which are neatly laid out thanks to the flow
    utility.

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,13 @@
+const markdownIt = require('markdown-it')
+
 module.exports = (config) => {
   config.addPassthroughCopy('src/images')
   config.addPassthroughCopy('src/index.js')
+
+  // Add filter to transform markdown to HTML
+  config.addFilter('markdown', (content) => {
+    return new markdownIt({ html: true }).render(content)
+  })
 
   return {
     htmlTemplateEngine: 'njk',
