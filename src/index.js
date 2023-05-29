abdfda866be8a6b2538033e59ea9dff94b4f4fbf
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2023-05-29 09:51:55 +0800

    Use aria-hidden to implement panel visibility
    The toggle animation added in commit bb83fee sets the accordion panels
    to `display: grid` which overrides the `hidden` attribute. This causes
    the content of the collapsed panels to be accessible to screen readers,
    thus introducing disparity between sighted and unsighted users.
    
    Since we have implemented our own logic for toggling visibility, we can
    switch to using `aria-hidden` for indicating state that is consistent
    for sighted and unsighted users.

--- src/index.js
+++ src/index.js
@@ -8,26 +8,27 @@ function togglePanel() {
   const panelId = this.getAttribute('aria-controls')
   const panel = document.getElementById(panelId)
 
-  // Toggle the controlled panel's hidden attribute
-  panel.hidden = !panel.hidden
-  // Toggle aria-expanded to reflect the current state
-  if (panel.hidden) {
-    this.setAttribute('aria-expanded', 'false')
-  } else {
+  // Toggle aria-expanded on button and aria-hidden on panel
+  if (panel.getAttribute('hidden') !== 'true') {
+    panel.setAttribute('aria-hidden', 'false')
     this.setAttribute('aria-expanded', 'true')
+  } else {
+    panel.setAttribute('aria-hidden', 'true')
+    this.setAttribute('aria-expanded', 'false')
   }
 }
 
 /**
  * Collapsing accordion panels requires two steps:
- * 1. setting the panel to hidden
+ * 1. setting the panel's aria-hidden to "true"
  * 2. updating the control's aria-expanded to "false"
  */
 function collapse(accordion) {
-  accordion.querySelector('.accordion-panel').hidden = true
-  accordion
-    .querySelector('.accordion-header-button')
-    .setAttribute('aria-expanded', 'false')
+  const panel = accordion.querySelector('.accordion-panel')
+  const button = accordion.querySelector('.accordion-header-button')
+
+  panel.setAttribute('aria-hidden', 'true')
+  button.setAttribute('aria-expanded', 'false')
 }
 
 function collapseAll(accordions) {
